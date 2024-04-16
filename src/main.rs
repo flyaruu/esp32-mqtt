aeab0782de1af52f30951094c723483005bdad29
Frank Lyaruu <frank.lyaruu@formelio.nl>
2024-04-16 07:44:12 +0200

    rust fmt

--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,38 @@
 #![feature(type_alias_impl_trait)]
 
 extern crate alloc;
-use core::mem::MaybeUninit;
 use alloc::{borrow::ToOwned, boxed::Box, format, string::String};
+use core::mem::MaybeUninit;
 use embassy_executor::task;
 use embassy_net::{Config, Stack, StackResources};
-use embassy_sync::{blocking_mutex::raw::NoopRawMutex, channel::{Channel, Receiver, Sender}};
+use embassy_sync::{
+    blocking_mutex::raw::NoopRawMutex,
+    channel::{Channel, Receiver, Sender},
+};
 use embassy_time::Timer;
 use esp_backtrace as _;
-use esp_hal::{clock::ClockControl, embassy::{self, executor::Executor}, peripherals::Peripherals, prelude::*, timer::TimerGroup};
+use esp_hal::{
+    clock::ClockControl,
+    embassy::{self, executor::Executor},
+    peripherals::Peripherals,
+    prelude::*,
+    timer::TimerGroup,
+};
 use esp_println::println;
 
-use esp_wifi::{initialize, wifi::{new_with_mode, WifiStaDevice}, EspWifiInitFor};
+use esp_wifi::{
+    initialize,
+    wifi::{new_with_mode, WifiStaDevice},
+    EspWifiInitFor,
+};
 
 use esp_hal::{systimer::SystemTimer, Rng};
 use net::run_network;
 
 use crate::{mqtt::send_mqtt, net::connect};
 
-mod net;
 mod mqtt;
-
+mod net;
 
 #[global_allocator]
 static ALLOCATOR: esp_alloc::EspHeap = esp_alloc::EspHeap::empty();
@@ -35,7 +47,7 @@ fn init_heap() {
         ALLOCATOR.init(HEAP.as_mut_ptr() as *mut u8, HEAP_SIZE);
     }
 }
-type Message = (String,String);
+type Message = (String, String);
 type MessageChannel = Channel<NoopRawMutex, Message, 10>;
 type MessageSender = Sender<'static, NoopRawMutex, Message, 10>;
 type MessageReceiver = Receiver<'static, NoopRawMutex, Message, 10>;
@@ -64,26 +76,33 @@ fn main() -> ! {
         &clocks,
     )
     .unwrap();
-    let wifi =  peripherals.WIFI;
-    let (device,controller) = new_with_mode(&init, wifi, WifiStaDevice).unwrap();
+    let wifi = peripherals.WIFI;
+    let (device, controller) = new_with_mode(&init, wifi, WifiStaDevice).unwrap();
     let dhcpconfig = Config::dhcpv4(Default::default());
     let stack_resource = Box::leak(Box::new(StackResources::<5>::new()));
-    let stack = Stack::new(device,dhcpconfig,stack_resource,3845834);
+    let stack = Stack::new(device, dhcpconfig, stack_resource, 3845834);
 
     let stack = Box::leak(Box::new(stack));
-    
+
     let executor = Box::leak(Box::new(Executor::new()));
     let timer_group = TimerGroup::new(peripherals.TIMG0, &clocks);
-    embassy::init(&clocks,timer_group);
+    embassy::init(&clocks, timer_group);
 
     let message_channel = Box::leak(Box::new(MessageChannel::new()));
 
-    
     executor.run(|spawner| {
         spawner.spawn(connect(controller)).unwrap();
         spawner.spawn(run_network(stack)).unwrap();
-        spawner.spawn(send_mqtt(stack,"mqtt.eclipseprojects.io", message_channel.receiver())).unwrap();
-        spawner.spawn(message_producer(message_channel.sender())).unwrap()
+        spawner
+            .spawn(send_mqtt(
+                stack,
+                "mqtt.eclipseprojects.io",
+                message_channel.receiver(),
+            ))
+            .unwrap();
+        spawner
+            .spawn(message_producer(message_channel.sender()))
+            .unwrap()
     })
 }
 
@@ -91,8 +110,10 @@ fn main() -> ! {
 async fn message_producer(sender: MessageSender) {
     let mut i = 0;
     loop {
-        sender.send(("floodplain".to_owned(),format!("Message #: {}",i))).await;
-        i+=1;
+        sender
+            .send(("floodplain".to_owned(), format!("Message #: {}", i)))
+            .await;
+        i += 1;
         Timer::after_secs(5).await;
     }
 }
--- src/net.rs
+++ src/net.rs
@@ -1,7 +1,10 @@
 use embassy_executor::task;
 use embassy_net::Stack;
 use embassy_time::Timer;
-use esp_wifi::wifi::{get_wifi_state, ClientConfiguration, Configuration, WifiController, WifiDevice, WifiStaDevice, WifiState};
+use esp_wifi::wifi::{
+    get_wifi_state, ClientConfiguration, Configuration, WifiController, WifiDevice, WifiStaDevice,
+    WifiState,
+};
 use log::info;
 
 const SSID: &str = env!("SSID");
@@ -12,49 +15,51 @@ pub async fn connect(mut controller: WifiController<'static>) {
     loop {
         info!("loop!");
         let state = get_wifi_state();
-        info!("Current state: {:?}",state);
+        info!("Current state: {:?}", state);
         match state {
-            esp_wifi::wifi::WifiState::StaConnected=>{
+            esp_wifi::wifi::WifiState::StaConnected => {
                 info!("connected");
-                controller.wait_for_event(esp_wifi::wifi::WifiEvent::StaDisconnected).await;
+                controller
+                    .wait_for_event(esp_wifi::wifi::WifiEvent::StaDisconnected)
+                    .await;
                 Timer::after_secs(5).await;
-            },
+            }
             esp_wifi::wifi::WifiState::StaStarted => info!("Started"),
             esp_wifi::wifi::WifiState::StaDisconnected => info!("Disconnected"),
-            esp_wifi::wifi::WifiState::StaStopped =>info!("Stopped"),
+            esp_wifi::wifi::WifiState::StaStopped => info!("Stopped"),
             _ => {}
         }
         let state = get_wifi_state();
-        info!("Current state: {:?}",state);
-        match state  {
-            WifiState::Invalid | WifiState::StaStopped=> {
+        info!("Current state: {:?}", state);
+        match state {
+            WifiState::Invalid | WifiState::StaStopped => {
                 let client_config = Configuration::Client(ClientConfiguration {
-                    ssid : SSID.try_into().unwrap(),
-                    password : PASSWORD.try_into().unwrap(),
+                    ssid: SSID.try_into().unwrap(),
+                    password: PASSWORD.try_into().unwrap(),
                     ..Default::default()
                 });
                 controller.set_configuration(&client_config).unwrap();
                 info!("About to start");
                 controller.start().await.unwrap();
                 info!("Started");
-            },
+            }
             _ => {}
         }
 
         let state = get_wifi_state();
-        info!("Now: {:?}",state);
+        info!("Now: {:?}", state);
 
         match controller.connect().await {
             Ok(_) => info!("Wifi up"),
             Err(e) => {
-                info!("wifi down: {:?}",e);
+                info!("wifi down: {:?}", e);
                 Timer::after_millis(500).await;
-            },
+            }
         }
-   }
+    }
 }
 
 #[task]
-pub async fn run_network(stack: &'static Stack<WifiDevice<'static,WifiStaDevice>> ) {
+pub async fn run_network(stack: &'static Stack<WifiDevice<'static, WifiStaDevice>>) {
     stack.run().await;
 }
